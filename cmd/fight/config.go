09f53abcaf2bfdec1c80fe8a211d09b6aaf96c93
Marc Spiteri <mrcsptr@gmail.com>
2018-03-04 21:54:47 +0100

    Implement config.Check

--- cmd/fight/config.go
+++ cmd/fight/config.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"encoding/json"
+    "errors"
 	"io/ioutil"
 )
 
 type Config struct {
-	SomeNumber int
-	SomeString string
+	TeamsLocation string
+	DudesLocation string
+	ATeam string
+	BTeam string
 }
 
 func NewConfig(filepath string) (Config, error){
@@ -15,14 +18,34 @@ func NewConfig(filepath string) (Config, error){
   if err != nil {
     return Config{}, err
   }
-
+  
   var c Config
   if err := json.Unmarshal(raw, &c); err != nil {
     return Config{}, err
   }
+  
+  if err := c.Check(); err!= nil {
+      return Config{}, err
+  }
+  
   return c, nil
 }
 
+func (c Config) Check () error{
+    if c.TeamsLocation==""{
+        return errors.New("undefined TeamsLocation")
+    }
+    if c.DudesLocation==""{
+        return errors.New("undefined DudesLocation")
+    }
+    if c.ATeam==""{
+        return errors.New("undefined ATeam")
+    }
+    if c.BTeam==""{
+        return errors.New("undefined BTeam")
+    }
+    return nil
+}
 
 func (c Config) String() string {
 	raw, _ := json.MarshalIndent(c, "", "    ")
