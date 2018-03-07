4a81d027e77c7163c9df51ffb2ea8607f792b263
Marc Spiteri <mrcsptr@gmail.com>
2018-03-07 15:36:24 +0100

    remise sur rails

--- dude.go
+++ dude.go
@@ -6,36 +6,26 @@ import (
 
 type Dude struct {
  Name string
- Grades [][]string
-
+ Results []DudeResult
 }
 
-//CalcResult implements the calculation of DudeResult based on the arguments of DudeGrades
-func (d Dude) CalcResult  () {
-type DudeResult struct {
-        A int
-        M int
-        D int
+type DudeResult struct{
+        Position string
+        Score int
     }
-    var i int
-   for {
-       if i>len(d.Grades){
-           break
-       }
-       switch d.Grades[i][0] {
-           case "A":
-               DudeResult.A+=d.Grades[i][1]
-               i++
-           case "M":
-               DudeResult.M+=d.Grades[i][1]
-               i++
-           case "D":
-               DudeResult.D+=d.Grades[i][1]
-               i++
-       }
-    
-   }
-   return DudeResult
+
+const(
+    V = 3
+    T = 2
+    D = 1
+)
+
+func (d Dude) Sum() int {
+    var v int
+    for _, s := range d.Results {
+            v+= s.Score
+        }
+        return v
 }
 
 
